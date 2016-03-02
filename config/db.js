dc965bad82de244ece229764bdb874c5cd7cf5f5
Kashif Ahmed <Kashif Ahmed>
2016-03-02 22:50:03 +0500

    Change DB URL

--- config/db.js
+++ config/db.js
@@ -1,21 +1,20 @@
 'use strict';
 
-module.exports = function(db){
-  db.connect('mongodb://jb:kodevz@ds053597.mongolab.com:53597/job-finder-dev');
+module.exports = function(db) {
+  db.connect("Your DB URL");
 
 
-  db.connection.on('connected', function () {
+  db.connection.on('connected', function() {
     console.log('Mongoose default connection open to ');
   });
 
   // If the connection throws an error
-  db.connection.on('error',function (err) {
+  db.connection.on('error', function(err) {
     console.log('Mongoose default connection error: ' + err);
   });
 
   // When the connection is disconnected
-  db.connection.on('disconnected', function () {
+  db.connection.on('disconnected', function() {
     console.log('Mongoose default connection disconnected');
   });
 };
-
